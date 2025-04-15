a14e2c77e295fd732985ccd87f908b305698f680
Vishnu Teja <142686736+vishnutej000@users.noreply.github.com>
2025-04-15 11:07:49 +0530

    final piece

--- backend/index.js
+++ backend/index.js
@@ -20,7 +20,13 @@ const app = express()
 // to make input as json
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }))
+app.use(cors({ 
+   origin: [
+    "http://localhost:5173",                         // for local dev
+    "https://note-nu-opal.vercel.app",               // your Vercel frontend
+                          
+  ],
+  credentials: true }))
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000")
