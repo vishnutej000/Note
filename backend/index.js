b01aab9d5ca578a317eb387e8f85f8f83070b74a
Vishnu Teja <142686736+vishnutej000@users.noreply.github.com>
2025-04-15 11:13:06 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({ 
    origin: [
-    "http://localhost:5173",                         // for local dev
+                            // for local dev
     "https://note-nu-opal.vercel.app",               // your Vercel frontend
                           
   ],
