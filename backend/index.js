73f6a4020b6e471e1ba5f466f814295fef125e28
Vishnu Teja <142686736+vishnutej000@users.noreply.github.com>
2025-07-11 08:43:05 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -28,8 +28,9 @@ app.use(cors({
   ],
   credentials: true }))
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000")
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
 })
 
 // import routes
